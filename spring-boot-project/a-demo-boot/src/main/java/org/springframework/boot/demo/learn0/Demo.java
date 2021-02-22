eda4115426dd158ee2b5beb89179f01523ff1eb9
zj2626 <zhangj2626>
2021-02-22 13:37:26 +0800

    init

---
+++ spring-boot-project/a-demo-boot/src/main/java/org/springframework/boot/demo/learn0/Demo.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.demo.learn0;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("SUCCESS ....................................................");
+	}
+}
